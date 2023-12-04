53d6d038808dff7c3409dacff7232318e9092311
skfeat <128950274+skfeat@users.noreply.github.com>
2023-12-04 21:48:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,57 @@ app.use(express.json());
 app.use(cors());
 
 // Connection URI
-const uri = 'mongodb+srv://skfeat:Raj1775@cluster0.clqoh73.mongodb.net/RealTime';
+const uri = 'mongodb+srv://skfeat:Raj1775@cluster0.clqoh73.mongodb.net/RealTimeData';
 
 // Connect to the MongoDB cluster using Mongoose
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB using Mongoose');
 
     const yourSchema = new mongoose.Schema({
       view: { type: Number, default: 0 },
+      timeLeft: { type: Number, default: 5 * 60 } // 5 minutes in seconds
     });
 
     const YourModel = mongoose.model('YourModel', yourSchema);
+  
+    // Middleware to update timeLeft
+    const updateTimer = setInterval(async () => {
+      try {
+        // Find and update all documents in the collection
+      
+        const updatedDocuments = await YourModel.updateMany({}, { $inc: { timeLeft: -1 } });
+
+        // Reset timeLeft to 300 seconds (5 minutes) when it reaches 0
+        await YourModel.updateMany({ timeLeft: { $lte: 0 } }, { $set: { timeLeft: 300 } });
+        await YourModel.updateMany({ timeLeft: 1 }, { $set: { view: 0 } });
+        console.log(`TimeLeft values updated.`);
+      } catch (error) {
+        console.error('Error updating timeLeft:', error);
+      }
+    }, 1000); // Update every second
+
+    // Handle server shutdown to clear the interval
+    process.on('SIGINT', () => {
+      clearInterval(updateTimer);
+      process.exit();
+    });
 
     app.get('/updateview', async (req, res) => {
-        try {
-          // Find the document and update the 'view' field by incrementing it by 1
-          const updatedDocument = await YourModel.findOneAndUpdate(
-            {},
-            { $inc: { view: 1 } }, // Increment the 'view' field by 1
-            { new: true } // Return the modified document
-          );
-  
-          res.json({ message: 'View updated successfully', updatedDocument });
-        } catch (error) {
-          console.error('Error updating view:', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-        }
-      });
+      try {
+        // Find the document and update the 'view' field by incrementing it by 1
+        const updatedDocument = await YourModel.findOneAndUpdate(
+          {},
+          { $inc: { view: 1 } }, // Increment the 'view' field by 1
+          { new: true } // Return the modified document
+        );
 
+        res.json({ message: 'View updated successfully', updatedDocument });
+      } catch (error) {
+        console.error('Error updating view:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
     app.get('/getdata', async (req, res) => {
       try {
@@ -49,30 +71,21 @@ mongoose.connect(uri)
       }
     });
 
-app.post('/createDocument', async (req, res) => {
-  try {
-    // Create a new document with the default values
-    const newDocument = new YourModel();
-    
-    // Save the new document to the database
-    const savedDocument = await newDocument.save();
+    app.post('/createDocument', async (req, res) => {
+      try {
+        // Create a new document with the default values
+        const newDocument = new YourModel();
+        
+        // Save the new document to the database
+        const savedDocument = await newDocument.save();
 
-    res.json({ message: 'Document created successfully', savedDocument });
-  } catch (error) {
-    console.error('Error creating document:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-setInterval(async () => {
-  try {
-    // Find and update all documents in the collection
-    const updatedDocuments = await YourModel.updateMany({}, { $set: { view: 0 } });
+        res.json({ message: 'Document created successfully', savedDocument });
+      } catch (error) {
+        console.error('Error creating document:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
-    console.log(`View values reset to zero.`);
-  } catch (error) {
-    console.error('Error resetting view values:', error);
-  }
-},60*60*1000); 
     app.listen(3000, () => {
       console.log(`Server is running on http://localhost:3000`);
     });
